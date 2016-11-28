c270ee24c6914f1108541845f6288f3ad63eb8f4
Anirudh Mukundan <anirudh@aiyengarG9.local>
2016-11-28 00:12:16 -0500

    initial ConvNet complete

--- cnn.py
+++ cnn.py
@@ -3,22 +3,25 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from util import load_data
-
-X_train, y_train = load_data('./train', 'train.csv')
+from keras.utils.np_utils import to_categorical
 
 # dimensions of our images.
 img_width, img_height, img_channels = 128, 128, 3
-nb_train_samples = 2000
-nb_validation_samples = 800
+nb_train_samples = 7000
+nb_classes = 8
 nb_epoch = 50
+batch_size = 32
 
-'''
+# load training data
+X_train, y_train = load_data('./train', 'train.csv')
 
-img = load_img('data/train/cats/cat.0.jpg')  # this is a PIL image
+# convert data to one-hot
+y_train = to_categorical(y_train - 1, nb_classes=nb_classes) 
 
-#build convnet
+# build ConvNet
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(img_channels, img_width, img_height)))
+
+model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height, img_channels)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -40,4 +43,14 @@ model.add(Activation('sigmoid'))
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-'''
+
+model.fit(X_train, y_train,
+          nb_epoch=nb_epoch,
+          batch_size=batch_size)
+
+score = model.evaluate(X_train, y_train, batch_size=batch_size)
+
+print('score')
+print(score)
+
+model.save_weights('weights.h5')
